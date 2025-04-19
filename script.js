3ed99028de67469377014faa4102725b270f6d5b
marrriiooo <mariosenawati@gmail.com>
2025-04-19 17:13:43 +0700

    menambahkan navigasi bar

--- script.js
+++ script.js
@@ -2,3 +2,12 @@
 const nama = document.querySelector(".nama");
 const huruf = [...nama.textContent].map((h) => `<span>${h}</span>`).join("");
 nama.innerHTML = huruf;
+
+const menuToggle = document.querySelector(" .menu-toggle input");
+const nav = document.querySelector("nav ul");
+
+menuToggle.addEventListener("click", function () {
+  nav.classList.toggle("slide");
+});
+
+// ketika klik di luar beground humberger keluar
